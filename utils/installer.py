33f3f0bef59fe8620ec0006ec6fd249fa0e71e1c
a karelse <aapskarel@gmail.com>
2015-02-10 06:42:55 +0100

    more installer work

--- utils/installer.py
+++ utils/installer.py
@@ -8,16 +8,24 @@ import imp
 
 url = urlopen("https://github.com/aaps/stardog/archive/master.zip")
 zipfile = ZipFile(StringIO(url.read()))
-zipfile.extractall("..")
-
-for filename in os.listdir(".."):
-	if filename.startswith("stardog"):
-		os.rename(filename, "stardog")
 
 
-try:
-    imp.find_module('eggs')
-    found = True
-except ImportError:
-    found = False
+names = zipfile.namelist()
+	
+for name in names:
+
+	newname = "/".join(name.split('/')[1:])
+	if newname.endswith('/'):
+		os.makedirs(newname)
+	elif len(newname) > 0:
+		file(newname, 'wb').write(zipfile.read(name))
+
+
+
+
+# try:
+#     imp.find_module('eggs')
+#     found = True
+# except ImportError:
+#     found = False
 
