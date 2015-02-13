ede525c9831420f3a98344d3b73757faf9ea8db6
a karelse <aapskarel@gmail.com>
2015-02-13 20:12:21 +0100

    better installer, not downloading the .git and the .gitignore

--- utils/installer.py
+++ utils/installer.py
@@ -12,8 +12,9 @@ names = zipfile.namelist()
 for name in names:
 
 	newname = "/".join(name.split('/')[1:])
-	if newname.endswith('/'):
-		os.makedirs(os.path.normpath(newname))
-	elif len(newname) > 0:
-		file(os.path.normpath(newname), 'wb').write(zipfile.read(name))
+	if newname.beginswith('.'):
+		if newname.endswith('/'):
+			os.makedirs(os.path.normpath(newname))
+		elif len(newname) > 0:
+			file(os.path.normpath(newname), 'wb').write(zipfile.read(name))
 
