603141720a030a7bdd0a108d34be4cc73e35076a
Robert <201292@live.nl>
2015-02-14 18:25:45 +0100

    making the sources pep8 compliant, will make alot of serious python coders a bit happier

--- code/commandParse.py
+++ code/commandParse.py
@@ -2,7 +2,9 @@ from types import *
 from spaceship import *
 from parts import *
 from vec2d import Vec2d
-import sys, os
+import sys
+import os
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -17,64 +19,71 @@ class bcolors:
 # GREEN = (0,255,0)
 # BLUE = (0,0,255)
 
+
 class AttrFilter(object):
     def __init__(self):
         parseFilterInput = "parseFilter.txt"
         path = os.getcwd()+"/code/"+parseFilterInput
         self.fconfig = open(path, "r")
+
     def getFiltered(self):
         filtered = []
         for line in self.fconfig.readlines():
-            #remove \r\n from input.
+            # remove \r\n from input.
             line = ''.join(line.split())
             filtered.append(line)
         return filtered
 
+
 class CommandParse(object):
     helpText = [
-    "!help [shows this help text]"
-    "!print <object...> <attributes> <...> [for example: !print game.player , or !print game.averagefps ]\n"
-    "!set <object.attr> <value> [sets object attribute to value]\n"
-    "!reload [invokes a reload of the command parse code. so coding is easier and can be tested while in game]\n"
-    "!exit [exits the game]\n"
-    "!insertPart <part> <amount> [inserts a amount of parts into the players inventory]\n"
-    "!parts [lists parts available W.I.P]\n"
-    "!removeItem <part> <amount> [removes a amount of parts from the player inventory W.I.P]\n"
-    "!func <code> [runs python code] \n"
-    "!setTextColor <color> [sets the print out text to color in formate (r,g,b) \n"
-    "!setShipColor <color> [colors your ship to the specified color W.I.P]\n"
-    "!convertShip <ship type> [convert your ship to ship type W.I.P]\n"
-    "!printdbg [toggles debug printing to the ingame console]\n"
+                "!help [shows this help text]"
+                "!print <object...> <attributes> <...> [for example: !print game.player , or !print game.averagefps ]\n"
+                "!set <object.attr> <value> [sets object attribute to value]\n"
+                "!reload [invokes a reload of the command parse code. so coding is easier and can be tested while in game]\n"
+                "!exit [exits the game]\n"
+                "!insertPart <part> <amount> [inserts a amount of parts into the players inventory]\n"
+                "!parts [lists parts available W.I.P]\n"
+                "!removeItem <part> <amount> [removes a amount of parts from the player inventory W.I.P]\n"
+                "!func <code> [runs python code] \n"
+                "!setTextColor <color> [sets the print out text to color in formate (r,g,b) \n"
+                "!setShipColor <color> [colors your ship to the specified color W.I.P]\n"
+                "!convertShip <ship type> [convert your ship to ship type W.I.P]\n"
+                "!printdbg [toggles debug printing to the ingame console]\n"
     ]
-    
+
     def __init__(self, game, chatconsole, messenger):
         self.game = game
         self.player = game.player
         self.messenger = messenger
         self.chatconsole = chatconsole
 
-        self.textColor = (244,244,200)
-        #a reference to a function that gets the text from the chatconsole
+        self.textColor = (244, 244, 200)
+        # a reference to a function that gets the text from the chatconsole
         self.getText = self.chatconsole.console.inputfield.getText
-        #a reference to a function that is able to set that text.
-        #for for example auto complete (future)
+        # a reference to a function that is able to set that text.
+        # for for example auto complete (future)
         self.setText = self.chatconsole.console.inputfield.setText
         self.text = []
         self.reload = False
         self.debug = False
+
     def setColor(self, args):
         if not args:
             return
         self.textColor = eval(args[0])
+
     def setShipColor(self, args):
         if not args:
             return
         raise Exception("Exception: not implemented yet")
+
     def convertShip(self, args):
         shipType = args[0]
         if shipType == "Scout":
             pass
         raise Exception("Exception: Not implemented.")
+
     def insertPart(self, args):
         if not args:
             return
@@ -83,12 +92,13 @@ class CommandParse(object):
         if len(args) == 1:
             part = args[0]
         else:
-            part,amount = args[0],int(args[1])
+            part, amount = args[0], int(args[1])
         self.player.insertPart(eval(part), amount)
-        self.printout("inserted %d %s's into inventory."%(amount, str(part)))
+        self.printout("inserted %d %s's into inventory." % (amount, str(part)))
 
     def handleInput(self, event):
         pass
+
     def printout(self, text):
         text = str(text)
         if '\n' in text:
@@ -97,25 +107,26 @@ class CommandParse(object):
                 self.messenger.message(str(item), self.textColor)
         else:
             self.messenger.message(str(text), self.textColor)
-    #print a single value,
-    #also check if that value is in the filter list.
-    #if so don't print it.
+    # print a single value,
+    # also check if that value is in the filter list.
+    # if so don't print it.
+
     def printAttrVal(self, attribute, value):
-        #attrfilter = AttrFilter()
+        # attrfilter = AttrFilter()
         filtered_list = AttrFilter().getFiltered()
         if str(attribute) not in filtered_list:
             self.printout(str(attribute)+" = "+str(value))
-        
+
     def printAttributes(self, obj):
         for element in obj.__dict__:
             self.printAttrVal(element, obj.__dict__[element])
-        
+
     def execFunc(self, args):
         try:
-            exec ' '.join(args)
-        except Exception, e:
+            exec(' '.join(args))
+        except Exception as e:
             self.printout(e)
-        
+
     def printFunc(self, args):
         if not args:
             return
@@ -126,65 +137,69 @@ class CommandParse(object):
         except:
             self.printout("invalid input")
             return
-        #if only one argument just print that ones value or attribute list.
+        # if only one argument just print that ones value or attribute list.
         if len(args) == 1:
             try:
                 self.printAttributes(attribute)
-            except Exception, e:
+            except Exception as e:
                 self.printAttrVal(last_argument, getattr(self, last_argument))
-        #else if we got more arguments traverse list and print value/attribute list.
+        # else if we got more arguments traverse list
+        # and print value/attribute list.
         else:
             for index in range(1, len(args)-1):
                 attribute = getattr(attribute, args[index])
             try:
                 self.printAttributes(getattr(attribute, last_argument))
-            except Exception, e:
-                self.printAttrVal(last_argument, getattr(attribute, last_argument) )
+            except Exception as e:
+                self.printAttrVal(last_argument,
+                                  getattr(attribute, last_argument))
         self.printout("")
-    
+
     def setFunc(self, args):
         if not args:
             return
         try:
-            temp = args[0].split('.') #split the attributes
+            # split the attributes
+            temp = args[0].split('.')
             temp.append(args[1])
-            args = temp #rejoin the attributes with the arguments.
+            # rejoin the attributes with the arguments.
+            args = temp
         except:
             self.printout("Invalid input")
             return
         attribute = getattr(self, args[0])
         last_argument = args[-1:][0]
         sec_last_argument = args[-2:][0]
-        
+
         if len(args) == 2:
             setattr(attribute, sec_last_argument, eval(last_argument))
         else:
             for index in xrange(1, len(args)-2):
                 attribute = getattr(attribute, args[index])
-                self.printout("attr: %s, arg: %s"%(str(attribute), args[index]))
-            setattr(attribute, sec_last_argument, eval(last_argument) )
-    
+                self.printout("attr: %s, arg: %s" % (str(attribute),
+                              args[index]))
+            setattr(attribute, sec_last_argument, eval(last_argument))
+
     def update(self):
-        #get console input
+        # get console input
         text = self.getText()
         if text:
             try:
-                #if the first character is a ! then it's a command
+                # if the first character is a ! then it's a command
                 if text[0] == '!':
-                    #remove the ! and split the text up in a list of words.
+                    # remove the ! and split the text up in a list of words.
                     text = text[1:].split(' ')
-                    #remove any to many spaces. or empty slots.
+                    # remove any to many spaces. or empty slots.
                     while '' in text:
                         text.remove('')
-                    #extract the command from the text.
+                    # extract the command from the text.
                     command = text[0]
-                    #extract arguments.
-                    #if arguments available.
+                    # extract arguments.
+                    # if arguments available.
                     if len(text) > 1:
                         args = text[1:]
                     else:
                         args = None
-                    #self.printout("input: %s \ncommand: %s \narguments: %s"%(text, command, args))
                     if command == 'print':
                         self.printFunc(args)
                     elif command == 'set':
@@ -214,12 +229,14 @@ class CommandParse(object):
                     else:
                         self.printout("Invalid input.")
                     if self.debug:
-                        self.printout("input: %s \ncommand: %s \narguments: %s"%(text, command, args))
+                        self.printout("input: %s \ncommand: %s \narguments: %s"
+                                      % (text, command, args))
                 else:
-                    self.printout(self.player.firstname + " " + self.player.secondname +": "+text)
-            except Exception, e:
-                #self.printout(e)
+                    self.printout(self.player.firstname + " " +
+                                  self.player.secondname + ": "+text)
+            except Exception as e:
+                # self.printout(e)
                 exc_type, exc_obj, exc_tb = sys.exc_info()
                 fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-                print e
+                print(e)
                 print(exc_type, str(fname)+":"+str(exc_tb.tb_lineno))
