764b52cb4fd502a24b6e0a382428a5b62abec7ce
Robert <201292@live.nl>
2015-02-14 19:04:46 +0100

    this¨

--- code/parts.py
+++ code/parts.py
@@ -244,29 +244,29 @@ class Part(Floater):
         
     def update(self):
         """updates this part."""
-        # if self.ship:
-        #reset so this part can act again this frame:
-        self.acted = False
-        #if it's attached to a ship, just rotate with the ship:
-        if self.parent:
-            cost = cos(self.ship.dir) #cost is short for cos(theta)
-            sint = sin(self.ship.dir)
-            self.pos = self.ship.pos + self.offset.rotated(self.ship.dir)
-        #if it's floating in space, act like a floater:
-        else:
-            Floater.update(self)
-        #update children:
-        #
-        for port in self.ports:
-            if port.part:
-                port.part.update()
+        if self.ship:
+            #reset so this part can act again this frame:
+            self.acted = False
+            #if it's attached to a ship, just rotate with the ship:
+            if self.parent:
+                cost = cos(self.ship.dir) #cost is short for cos(theta)
+                sint = sin(self.ship.dir)
+                self.pos = self.ship.pos + self.offset.rotated(self.ship.dir)
+            #if it's floating in space, act like a floater:
+            else:
+                Floater.update(self)
+            #update children:
+            #
+            for port in self.ports:
+                if port.part:
+                    port.part.update()
 
-        if self.pickuptimeout > 0:
-            self.pickuptimeout -= 1. / self.fps
+            if self.pickuptimeout > 0:
+                self.pickuptimeout -= 1. / self.fps
 
-        for emitter in self.emitters:
-            emitter.setFPS(self.fps)
-            emitter.update()
+            for emitter in self.emitters:
+                emitter.setFPS(self.fps)
+                emitter.update()
 
 
     def draw(self, surface, offset = None, redraw = True, grey = False):
