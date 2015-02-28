d0b210ca59da2b702706864344738b72afc764b6
a karelse <aapskarel@gmail.com>
2015-02-28 22:34:24 +0100

    some extrabackupper work

---
+++ installer-utils/backuper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import os
+import time
+import urllib2
+import datetime
+import glob
+
+files = []
+formatstring = "%A-%d-%B-%Y"
+
+timestampmax = datetime.datetime.now() + datetime.timedelta(days=64)
+
+for filename in os.listdir("."):
+	if filename[-4:] == ".zip":
+		if datetime.datetime.strptime(filename.split("_")[1], formatstring) > timestampmax:
+			os.remove("./" + filename)
+
+
+response = urllib2.urlopen('https://github.com/aaps/stardog/archive/master.zip')
+afile = response.read()
+open("stardog_" + now.strftime(formatstring)  + "_.zip", "w").write(afile)
