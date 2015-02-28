8ad4b635f45ae60e48045be2ed727601ca2b39e2
a karelse <aapskarel@gmail.com>
2015-02-28 23:52:11 +0100

    more fixes, better cron support i hope

--- installer-utils/backuper.py
+++ installer-utils/backuper.py
@@ -6,17 +6,19 @@ import urllib2
 import datetime
 import glob
 
+path = "/root/temp/"
+
 files = []
 formatstring = "%A-%d-%B-%Y"
 now = datetime.datetime.now()
 timestampmax = now + datetime.timedelta(days=64)
 
-for filename in os.listdir("."):
+for filename in os.listdir(path):
 	if filename[-4:] == ".zip":
 		if datetime.datetime.strptime(filename.split("_")[1], formatstring) > timestampmax:
-			os.remove("./" + filename)
+			os.remove("path" + filename)
 
 
 response = urllib2.urlopen('https://github.com/aaps/stardog/archive/master.zip')
 afile = response.read()
-open("stardog_" + now.strftime(formatstring)  + "_.zip", "w").write(afile)
+open(path + "stardog_" + now.strftime(formatstring)  + "_.zip", "w").write(afile)
